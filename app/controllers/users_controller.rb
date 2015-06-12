7672efcadd9701355bbfbf19f890f1fbbb84a44d
Markus Olsen <molsen13@gmail.com>
2015-06-12 07:48:48 -0600

    Add new user's id to session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
 
     if user.save
+      session[:user_id] = user.id
       flash[:notice] = "Welcome, #{user.name}!"
       redirect_to root_path
     else
