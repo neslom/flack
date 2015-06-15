8fcc1fd89e2ad44b1c79d1951f1856cf4661a485
Markus Olsen <molsen13@gmail.com>
2015-06-14 19:50:52 -0600

    Set up a has many relationship for user to messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150615011953) do
+ActiveRecord::Schema.define(version: 20150615014618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150615011953) do
     t.text     "channel"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
