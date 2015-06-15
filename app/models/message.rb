8da292320fae104f6ce6adb87409f14d0a06522a
Markus Olsen <molsen13@gmail.com>
2015-06-14 19:30:32 -0600

    Message is invalid with no body

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,3 @@
 class Message < ActiveRecord::Base
-
+  validates :body, presence: true
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,12 +1,20 @@
 require "rails_helper"
 
 RSpec.describe Message do
-  context "is valid" do
+  context "is valid if" do
     it "has a body and a channel" do
       message = Message.create(body: "message body", channel: "main")
 
       expect(message.body).to eq("message body")
       expect(message.channel).to eq("main")
+    end
+  end
+
+  context "is invalid if" do
+    it "does not have a body" do
+      expect do
+        Message.create!(body: "", channel: "main")
+      end.to raise_error(ActiveRecord::RecordInvalid, /Body can't be blank/)
     end
   end
 end
