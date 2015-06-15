14ce31da53c34b52ad0380aaab05b8f70e72782a
Markus Olsen <molsen13@gmail.com>
2015-06-14 19:26:31 -0600

    Generated messages migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150612024605) do
+ActiveRecord::Schema.define(version: 20150615011953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "body"
+    t.text     "channel"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.text     "name"
