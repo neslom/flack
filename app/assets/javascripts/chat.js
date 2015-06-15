bceb6e5884731f8c7177c2301e8535a5644d1f84
Markus Olsen <molsen13@gmail.com>
2015-06-15 17:44:17 -0600

    Currently appending a message into the chat box

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,14 +1,33 @@
 $(document).ready(function () {
   var $submitButton = $('.submit');
   var $message = $('.message');
+  var $messages = $('.messages');
 
   $submitButton.on('click', function () {
+    var channel = $message.data('channel');
+    var messageBody = $message.val();
     $.ajax({
-      method: "POST",
-      url: "/messages",
+      method: 'POST',
+      url: '/messages',
+      dataType: 'json',
+      data: { message: { body: messageBody, channel: channel } },
       success: function (data) {
-        alert(data);
+        appendMessageToRoom($messages, data);
       }
     });
   });
 });
+
+function appendMessageToRoom (element, data) {
+  element.append('<p>' + data.message.body + ' [' + data.user + '] [' + formatDate(data.message.created_at) + ']</p>');
+};
+
+function formatDate (date) {
+  var dateObj = new Date(date);
+  var options = {
+    weekday: "long", year: "numeric", month: "short",
+    day: "numeric", hour: "2-digit", minute: "2-digit"
+  };
+
+  return dateObj.toLocaleTimeString('en-US', options);
+};
