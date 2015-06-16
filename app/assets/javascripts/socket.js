15aa5a37ad692c0776fb8fe5a76bd6c70ab7777c
Markus Olsen <molsen13@gmail.com>
2015-06-16 16:27:25 -0600

    Connecting to differing sockets based on the path

--- app/assets/javascripts/socket.js
+++ app/assets/javascripts/socket.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
-  var socket = io('http://localhost:3000');
+  var mainSocket = io('http://localhost:3000/main');
 
-  socket.on('message', function (message) {
+  //alert(window.location.pathname);
+
+  mainSocket.on('message', function (message) {
     console.log(message);
   });
 });
