6f33a4105c695dce7be9ef1ac0aac9b1e20c32de
Markus Olsen <molsen13@gmail.com>
2015-06-16 16:55:34 -0600

    Fixed teaspoon and magiclamp test config

--- spec/javascripts/append_message_spec.js
+++ spec/javascripts/append_message_spec.js
@@ -2,11 +2,11 @@ var expect = chai.expect;
 
 describe('Append message to chatroom', function () {
   beforeEach(function () {
-    MagicLamp.load('main/index');
+    MagicLamp.load('main_chat/index');
   });
 
   it('returns the appropriate string of html', function () {
-    var data = { message: {id: 43, body: "hey", channel: "main" }, user: "markus" };
+    var data = { message: { id: 43, body: "hey", channel: "main" }, user: "markus" };
 
     expect($('.messages').val()).to.eq("");
 
@@ -14,12 +14,7 @@ describe('Append message to chatroom', function () {
 
     expect($('.messages').text()).to.include("hey");
     expect($('.messages').text()).to.include("markus");
+    expect($('.message').text()).to.be.empty;
   });
 
-  xit('formats the data correctly', function () {
-    // the date formatting function appears to only work in the browser
-    var date = "2015-06-16T00:16:33.703Z";
-
-    expect(formatDate(date)).to.eq('Monday, Jun 15, 2015, 6:16 PM');
-  });
 });
