7c99bde2df69be65b4cd52b0bcd3355cb48e871c
Markus Olsen <molsen13@gmail.com>
2015-06-15 18:27:39 -0600

    Built date formatting test, but it appears that toLocaleTime only works in the browser

--- spec/javascripts/append_message_spec.js
+++ spec/javascripts/append_message_spec.js
@@ -6,11 +6,20 @@ describe('Append message to chatroom', function () {
   });
 
   it('returns the appropriate string of html', function () {
-    expect($('.messages').val()).to.eq("");
     var data = { message: {id: 43, body: "hey", channel: "main" }, user: "markus" };
 
+    expect($('.messages').val()).to.eq("");
+
     appendMessageToRoom($('.messages'), data);
+
     expect($('.messages').text()).to.include("hey");
     expect($('.messages').text()).to.include("markus");
+  });
+
+  xit('formats the data correctly', function () {
+    // the date formatting function appears to only work in the browser
+    var date = "2015-06-16T00:16:33.703Z";
+
+    expect(formatDate(date)).to.eq('Monday, Jun 15, 2015, 6:16 PM');
   });
 });
