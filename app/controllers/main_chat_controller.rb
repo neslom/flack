59d8d301ea18105ca9c3047cd664b8b985e62e32
Markus Olsen <molsen13@gmail.com>
2015-06-18 13:43:07 -0600

    Move MainChatController to use refactored last_five_chat_messages method

--- app/controllers/main_chat_controller.rb
+++ app/controllers/main_chat_controller.rb
@@ -2,6 +2,6 @@ class MainChatController < ApplicationController
   respond_to :html, :json
 
   def index
-    respond_with Message.last_five_main_chat_messages
+    respond_with Message.last_five_chat_messages("main")
   end
 end
