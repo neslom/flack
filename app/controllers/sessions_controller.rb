3ec353b1b392af9860499b15d882ff406ae896ed
Markus Olsen <molsen13@gmail.com>
2015-06-19 08:32:21 -0600

    Move soundcloud client creation to initializer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,23 @@
 class SessionsController < ApplicationController
   def new
+    redirect_to $client.authorize_url()
   end
 
   def create
-    user = User.find_by(email: params[:session][:email])
-
-    if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.id
-      flash[:notice] = "Welcome, #{user.name}"
-      redirect_to main_chat_path
-    else
-      flash[:danger] = "Invalid login attempt"
-      redirect_to login_path
+    if params[:code]
+      access_token = $client.exchange_token(code: params[:code])
     end
+
+    #user = User.find_by(email: params[:session][:email])
+
+    #if user && user.authenticate(params[:session][:password])
+      #session[:user_id] = user.id
+      #flash[:notice] = "Welcome, #{user.name}"
+      #redirect_to main_chat_path
+    #else
+      #flash[:danger] = "Invalid login attempt"
+      #redirect_to login_path
+    #end
   end
 
   def destroy
